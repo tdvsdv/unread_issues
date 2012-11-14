b3aab20626c2697c1064472913ea3ca69356a2c4
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2012-11-14 15:26:18 +1100

    Issue #4418. Unread issues now faster a little.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,3 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+get 'counts/user/issues/', :controller => 'counts', :action => 'user_issues'
--- lib/unread_issues/issue_patch.rb
+++ lib/unread_issues/issue_patch.rb
@@ -1,44 +1,37 @@
 module UnreadIssues
   module IssuePatch
+
     def self.included(base)
-		base.extend(ClassMethods)
-		base.send(:include, InstanceMethods)	
+      base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)  
 
-		base.class_eval do
-			has_many :issue_reads, :dependent=>:delete_all
-			alias_method_chain :css_classes, :unread_issues
-		end
-		
+      base.class_eval do
+        has_many :issue_reads, :dependent=>:delete_all
+        has_one :user_read, :class_name => 'IssueRead', :foreign_key => 'issue_id', :conditions => lambda{|*args| "user_id=#{User.current.id}" }
+        alias_method_chain :css_classes, :unread_issues
+      end
     end
-	
-	module ClassMethods   
-	
-	end
-	
-	module InstanceMethods
-		def css_classes_with_unread_issues
-			s=css_classes_without_unread_issues
-			s << ' unread' if unread?
-			s << ' updated' if updated?
-			s
-		end
-
-		def unread?
-			if issue_reads.count(:conditions => ["user_id=?", User.current.id])>0 or assigned_to_id != User.current.id or closed?
-				return false
-			else
-			    return true
-			end
-		end	
-
-		def updated?
-			if issue_reads.count(:conditions => ["read_date < ? AND user_id=?", updated_on, User.current.id])>0 and assigned_to_id == User.current.id and not closed?
-				return true
-			else
-			    return false
-			end
-		end
-	end
-	
+  
+    module ClassMethods    
+    end
+  
+    module InstanceMethods
+      def css_classes_with_unread_issues
+        s = css_classes_without_unread_issues
+        s << ' unread' if unread?
+        s << ' updated' if updated?
+        s
+      end
+  
+      def unread?
+        not (!user_read.nil? or (assigned_to_id != User.current.id) or closed?)
+      end 
+  
+      def updated?
+        !user_read.nil? and (user_read.read_date < updated_on) and (assigned_to_id == User.current.id) and !closed?
+      end
+  
+    end
+  
   end
 end
