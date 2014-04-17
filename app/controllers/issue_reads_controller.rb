720697dcda359cfcb9a24d36141639bf472edb8f
Kovalevskiy Vasil <skif24-91@mail.ru>
2014-04-17 22:31:47 +1100

    counters, may be better, but have not time

--- app/controllers/issue_reads_controller.rb
+++ app/controllers/issue_reads_controller.rb
@@ -34,17 +34,56 @@ class IssueReadsController < ApplicationController
     data = { count: 0, affects: nil }
     result = [ ]
 
+    today = Date.today
+    days_dec = (settings[:deadline] || { })[:days_inc].to_i.days
+    old = (today - days_dec).strftime('%Y-%m-%d')
+    week = (today - days_dec + 7.days).strftime('%Y-%m-%d')
+    month = (today.end_of_month - days_dec).strftime('%Y-%m-%d')
+    data[:custom_link] = "<a href='#{my_page_path}'><span>#{l('unread_issues.magic_my_page.label_view')}</span></a>".html_safe
+
+    counts = nil
     case params[:type]
       when 'changes_in_issues'
-        data[:count] = 0
+        counts = Issue.joins(:status, :issue_reads)
+                      .where("#{IssueRead.table_name}.read_date < #{Issue.table_name}.updated_on
+                          and #{IssueRead.table_name}.user_id = :user_id
+                          and #{Issue.table_name}.assigned_to_id = :user_id
+                          and #{IssueStatus.table_name}.is_closed = :false_flag
+                             ", user_id: User.current.id, false_flag: false)
+                      .select("SUM(case when #{IssueRead.table_name}.read_date <= '#{old}' then 1 else 0 end) as old,
+                               SUM(case when #{IssueRead.table_name}.read_date > '#{old}' and #{IssueRead.table_name}.read_date <= '#{week}' then 1 else 0 end) as soon,
+                               SUM(case when #{IssueRead.table_name}.read_date > '#{week}' and #{IssueRead.table_name}.read_date <= '#{month}' then 1 else 0 end) as in_this_month,
+                               SUM(case when #{IssueRead.table_name}.read_date > '#{month}' then 1 else 0 end) as in_next_month
+                              ").first
       when 'new_issues'
-        data[:count] = 0
+        counts = Issue.joins(:status)
+                      .joins("LEFT JOIN #{IssueRead.table_name} ir on ir.issue_id = #{Issue.table_name}.id and ir.user_id = #{User.current.id}")
+                      .where("ir.read_date is null
+                          and #{Issue.table_name}.assigned_to_id = :user_id
+                          and #{IssueStatus.table_name}.is_closed = :false_flag
+                             ", user_id: User.current.id, false_flag: false)
+                      .select("SUM(case when #{Issue.table_name}.created_on <= '#{old}' then 1 else 0 end) as old,
+                               SUM(case when #{Issue.table_name}.created_on > '#{old}' and #{Issue.table_name}.created_on <= '#{week}' then 1 else 0 end) as soon,
+                               SUM(case when #{Issue.table_name}.created_on > '#{week}' and #{Issue.table_name}.created_on <= '#{month}' then 1 else 0 end) as in_this_month,
+                               SUM(case when #{Issue.table_name}.created_on > '#{month}' then 1 else 0 end) as in_next_month
+                              ").first
     end
 
-    result << data if (result.size == 0)
-    result.collect { |it|
-      it[:custom_link] ||= "<a href='#{url_for({ controller: :kpi_period_users, action: params[:type] })}'><span>#{l('label_' + params[:type].to_s)}</span></a>".html_safe
-    }
+    data[:affects] = today.strftime('%Y-%m-%d')
+    data[:count] = counts.nil? ? 0 : counts.attributes['old'].to_i
+    result << data.clone( )
+
+    data[:affects] = (today + 6.day).strftime('%Y-%m-%d')
+    data[:count] = counts.nil? ? 0 : counts.attributes['soon'].to_i
+    result << data.clone( )
+
+    data[:affects] = (today.end_of_month).strftime('%Y-%m-%d')
+    data[:count] = counts.nil? ? 0 : counts.attributes['in_this_month'].to_i
+    result << data.clone( )
+
+    data[:affects] = (today.end_of_month + 1.day).strftime('%Y-%m-%d')
+    data[:count] = counts.nil? ? 0 : counts.attributes['in_next_month'].to_i
+    result << data.clone( )
 
     mmp_render_counters(result, settings, url_for(only_path: true))
   end
