d973c5965bfbab30ce724c7c9fe02dda52321f90
Kovalevskiy Vasil <skif24-91@mail.ru>
2014-12-12 15:42:08 +1000

    no message

--- lib/unread_issues/hooks_views.rb
+++ lib/unread_issues/hooks_views.rb
@@ -1,7 +1,8 @@
 module UnreadIssues
   module UnreadIssues
     class Hooks < Redmine::Hook::ViewListener
-      render_on :view_layouts_base_html_head, :partial => 'hooks/unread_isues/add_css', :layout => false
+      render_on(:view_layouts_base_html_head, partial: 'hooks/unread_isues/add_css', layout: false)
+      render_on(:view_issues_show_details_bottom, partial: 'hooks/unread_isues/issue_bottom_details', layout: false)
     end
   end
 end
